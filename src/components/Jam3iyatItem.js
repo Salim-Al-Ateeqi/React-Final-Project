591e40627a960307ab3919988603db3d8268702c
Your Name <you@example.com>
2021-10-18 19:29:55 +0300

    rendered fetch list

--- src/components/Jam3iyatItem.js
+++ src/components/Jam3iyatItem.js
@@ -1,9 +1,24 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
 
 function Jam3iyatItem({ jam3iya }) {
   return (
     <div>
-      <img src={jam3iya.image} alt="" />
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={jam3iya.image} />
+        <Card.Body>
+          <Card.Title>{jam3iya.title}</Card.Title>
+          <Card.Text>
+            <br>
+              {/* {jam3iya.amount}
+              {jam3iya.limit}
+              {jam3iya.startDate}
+              {jam3iya.endDate} */}
+            </br>
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
     </div>
   );
 }

45bf69ad4c7367f94d58563e86f3423e556300d4
CaptSoulBeard <Salim.Al.Ateeqi1@gmail.com>
2021-10-19 16:34:13 +0300

    clean up

--- src/App.js
+++ src/App.js
@@ -7,32 +7,12 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-  toast.error("Unauthorized!", {
-    position: "top-right",
-    autoClose: 5000,
-    hideProgressBar: false,
-    closeOnClick: true,
-    pauseOnHover: true,
-    draggable: true,
-    progress: undefined,
-  });
   return (
     <div className="App">
       <MyNavbar />
       <Home />
       <CreateJam3iyaButton />
       <Jam3iyatList />
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
     </div>
   );
 }
